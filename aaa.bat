f9c72b3d570bb652b6fc7903ef1bb140742d84b5
KitsuneeO <michal52176@gmail.com>
2026-08-07 16:32:16 +0200

    Update aaa.bat

--- aaa.bat
+++ aaa.bat
@@ -1,5 +1,1 @@
-@echo off
-title Wykryte kamery
-powershell -NoProfile -Command "Get-PnpDevice -Class Camera | Select-Object FriendlyName, Status | Format-Table -AutoSize"
-echo.
-pause
+curl parrot.live
