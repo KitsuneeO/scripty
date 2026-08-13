de062b67f60fe663011c19ccc08128364a9f90b3
KitsuneeO <michal52176@gmail.com>
2026-08-13 11:18:40 +0200

    Update aaa.bat

--- aaa.bat
+++ aaa.bat
@@ -1,1 +1,23 @@
-curl parrot.live
+@echo off
+title SYSTEM ERROR
+color 0a
+
+echo Initializing...
+timeout /t 2 >nul
+
+echo WARNING: SYSTEM COMPROMISED
+timeout /t 1 >nul
+
+start cmd /k "title HACKED & color 0c & echo TWOJ KOMPUTER ZOSTAL ZHAKOWANY XD"
+start cmd /k "title VIRUS DETECTED & color 0a & echo VIRUS DETECTED!!!"
+start cmd /k "color 0e & echo LOADING... & ping localhost -n 3 >nul & echo JUST KIDDING XD"
+
+echo ^G
+timeout /t 2 >nul
+
+cls
+color 0c
+echo.
+echo           YOU HAVE BEEN TROLLED XD
+echo.
+pause
